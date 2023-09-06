73d98d94194fbba01639c02d6cd1e6096dd5f93f
Piotr Poterek <peterpoterek0@gmail.com>
2023-09-06 23:28:23 +0200

    separate css files added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './css/style.css';
+
+import './css/about.css';
+import './css/advantages.css';
+import './css/contacts.css';
+import './css/customer-reviews.css';
+import './css/footer.css';
+import './css/gallery.css';
+import './css/hero-header.css';
+import './css/products.css';
