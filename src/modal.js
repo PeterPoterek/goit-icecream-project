cab053e67714150d01104f481fea111c3cdf7067
kcufury <137416296+kcufury@users.noreply.github.com>
2023-09-09 14:23:22 +0200

    Modals done + css
    Zrobione zostały modale chyba nie pominąłem żadnego... Oraz został stworzony modal.css w którym są wszystkie potrzebne style lecz jeszcze trzeba będzie zrobić Adaptative. Strzałka w <button> została pobrana i dodana do folderu images.

---
+++ src/modal.js
@@ -0,0 +1,25 @@
+const btns = document.querySelectorAll("[data-target]");
+const close_btns = document.querySelectorAll(".modal-btn");
+const overley = document.querySelector("#overley");
+
+btns.forEach(btn => { 
+    btn.addEventListener('click', () => { 
+        document.querySelector(btn.dataset.target).classList.add("active");
+        overley.classList.add('active');
+    })
+})
+
+close_btns.forEach(btn => { 
+    btn.addEventListener('click', () => { 
+        document.querySelector(btn.dataset.target).classList.remove("active");
+        overley.classList.remove('active');
+    })
+})
+
+window.onclick = (e) => {
+    if (e.target == overley) {
+        const modals = document.querySelectorAll(".modal");
+        modals.forEach((modal) => modal.classList.remove("active"));
+        overley.classList.remove('active');
+    }
+};
